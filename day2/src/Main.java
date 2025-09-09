076cee35bd67420c89d590b65d8adafbf16f8063
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-09-09 13:54:17 +0200

    day2 template for uppgifter

--- day2/src/Main.java
+++ day2/src/Main.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner choice = new Scanner(System.in);
+        boolean goOn = true;
+        while (goOn) {
+            System.out.println("""
+                    Please input task and parameters. The following are available:
+                    - "temp" - Temperaturanalys för en vecka
+                    - "student" - Studentbetyg för en klass
+                    - "utgifter" - Veckans utgifter
+                    - "priser" - Handelslista med priser
+                    - "tallek" - Enkel tallek
+                    - "exit" or "quit" - end the interaction
+                    """);
+            switch (choice.next()) {
+                case "temp":
+                    System.out.println("Not implemented yet");
+                    break;
+                case "student":
+                    System.out.println("Not implemented yet");
+                    break;
+                case "utgifter":
+                    System.out.println("Not implemented yet");
+                    break;
+                case "priser":
+                    System.out.println("Not implemented yet");
+                    break;
+                case "tallek":
+                    System.out.println("Not implemented yet");
+                    break;
+                case "exit", "quit":
+                    goOn = false;
+                    System.out.println("Good bye.");
+                    break;
+                default:
+                    System.out.println("Unexpected request");
+                    break;
+            }
+        }
     }
 }
